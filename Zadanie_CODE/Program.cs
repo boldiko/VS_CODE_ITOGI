db9e44edc50528e853b4180564329f5ae58585bc
unknown <boldinini38@mail.ru>
2023-06-27 18:30:59 +0300

    first commit

---
+++ Zadanie_CODE/Program.cs
@@ -0,0 +1,10 @@
+﻿﻿Console.WriteLine("Программа, которая выводит значения меньше или равное 3.");
+Console.Write("Введите через пробел значения: ");
+char[] separator = new char[] {' '};
+string[] st1 = Console.ReadLine()!.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+Console.Write(string.Join(", ", st1));
+Console.Write(" ->");
+for (int i = 0; i < st1.Length; i++)
+{
+    if (st1[i].Length <= 3) Console.Write($" {st1[i]},");
+}
