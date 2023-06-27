20ad5adaa49e7e706d08827930353fa01b770a1c
unknown <boldinini38@mail.ru>
2023-06-27 18:36:20 +0300

    first comit

--- Zadanie_CODE/Program.cs
+++ Zadanie_CODE/Program.cs
@@ -4,7 +4,3 @@ char[] separator = new char[] {' '};
 string[] st1 = Console.ReadLine()!.Split(separator, StringSplitOptions.RemoveEmptyEntries);
 Console.Write(string.Join(", ", st1));
 Console.Write(" ->");
-for (int i = 0; i < st1.Length; i++)
-{
-    if (st1[i].Length <= 3) Console.Write($" {st1[i]},");
-}
